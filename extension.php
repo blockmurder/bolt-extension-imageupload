fce864f8f5ad2c88ede1f46bc6d83111abaa91fd
blockmurder <info@blockmurder.ch>
2014-08-15 15:41:45 +0000

    code cleanup

--- extension.php
+++ extension.php
@@ -60,7 +60,7 @@ class Extension extends \Bolt\BaseExtension
         }
         if ($this->authorized){
             $this->path = $this->app['config']->get('general/branding/path') . '/extensions/image-upload';
-            $this->app->match($this->path, array($this, 'myfunction'));
+            $this->app->match($this->path, array($this, 'imageUpload'));
 
 
             $this->addMenuOption(__('ImageUpload'), $this->app['paths']['bolt'] . 'extensions/image-upload', "icon-list");
@@ -77,7 +77,7 @@ class Extension extends \Bolt\BaseExtension
      *
      * @return mixed
      */
-    public function myfunction()
+    public function imageUpload()
     {
  
         // Require a logged in user..
@@ -86,81 +86,6 @@ class Extension extends \Bolt\BaseExtension
         // Set up some vars.
         $title = "ImageUpload";
         $urlbase = $this->app['paths']['app'];
-        $form_template = '<script id="template-upload" type="text/x-tmpl">
-									{% for (var i=0, file; file=o.files[i]; i++) { %}
-									    <tr class="template-upload fade">
-									        <td>
-									            <span class="preview"></span>
-									        </td>
-									        <td>
-									            <p class="name">{%=file.name%}</p>
-									            <strong class="error text-danger"></strong>
-									        </td>
-									        <td>
-									            <p class="size">Processing...</p>
-									            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
-									        </td>
-									        <td>
-									            {% if (!i && !o.options.autoUpload) { %}
-									                <button class="btn btn-primary start" disabled>
-									                    <i class="glyphicon glyphicon-upload"></i>
-									                    <span>Start</span>
-									                </button>
-									            {% } %}
-									            {% if (!i) { %}
-									                <button class="btn btn-warning cancel">
-									                    <i class="glyphicon glyphicon-ban-circle"></i>
-									                    <span>Cancel</span>
-									                </button>
-									            {% } %}
-									        </td>
-									    </tr>
-									{% } %}
-									</script>
-									<!-- The template to display files available for download -->
-									<script id="template-download" type="text/x-tmpl">
-									{% for (var i=0, file; file=o.files[i]; i++) { %}
-									    <tr class="template-download fade">
-									        <td>
-									            <span class="preview">
-									                {% if (file.thumbnailUrl) { %}
-									                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
-									                {% } %}
-									            </span>
-									        </td>
-									        <td>
-									            <p class="name">
-									                {% if (file.url) { %}
-									                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?\'data-gallery\':\'\'%}>{%=file.name%}</a>
-									                {% } else { %}
-									                    <span>{%=file.name%}</span>
-									                {% } %}
-									            </p>
-									            {% if (file.error) { %}
-									                <div><span class="label label-danger">Error</span> {%=file.error%}</div>
-									            {% } %}
-									        </td>
-									        <td>
-									            <span class="size">{%=o.formatFileSize(file.size)%}</span>
-									        </td>
-									        <td>
-									            {% if (file.deleteUrl) { %}
-									                <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields=\'{"withCredentials":true}\'{% } %}>
-									                    <i class="glyphicon glyphicon-trash"></i>
-									                    <span>Delete</span>
-									                </button>
-									                <input type="checkbox" name="delete" value="1" class="toggle">
-									            {% } else { %}
-									                <button class="btn btn-warning cancel">
-									                    <i class="glyphicon glyphicon-ban-circle"></i>
-									                    <span>Cancel</span>
-									                </button>
-									            {% } %}
-									        </td>
-									    </tr>
-									{% } %}
-									</script>
-									';
  
         // add 'assets/' to the twigloader, so it can find the templates there.
         $this->app['twig.loader.filesystem']->addPath(__DIR__.'/assets/');
@@ -169,7 +94,6 @@ class Extension extends \Bolt\BaseExtension
         $html = $this->app['twig']->render('page.twig', array(
             'title' => $title,
             'content' => $content,
-            'form_template' => $form_template,
             'urlbase' => $urlbase,
 
         ));
@@ -207,47 +131,7 @@ class Extension extends \Bolt\BaseExtension
 
 <noscript><link rel='stylesheet' href='{urlbase}extensions/ImageUpload/css/jquery.fileupload-noscript.css'></noscript>
 <noscript><link rel='stylesheet' href='{urlbase}extensions/ImageUpload/css/jquery.fileupload-ui-noscript.css'></noscript>
-
-<script type='text/javascript' id='demo1-javascript'>
-$(document).ready(function() {
-	// Initialize navgoco with default options
-	$('#gallery_sel').navgoco({
-		caretHtml: '',
-		accordion: false,
-		openClass: 'open',
-		save: true,
-		cookie: {
-			name: 'navgoco',
-			expires: false,
-			path: '/'
-		},
-		slide: {
-			duration: 400,
-			easing: 'swing'
-		},
-		// Add Active class to clicked menu item
-		onClickAfter: function(e, submenu) {
-			e.preventDefault();
-			$('#gallery_sel').find('li').removeClass('active');
-			var li =  $(this).parent();
-			var lis = li.parents('li');
-			li.addClass('active');
-			lis.addClass('active');
-		},
-	});
-
-	$('#collapseAll').click(function(e) {
-		e.preventDefault();
-		$('#gallery_sel').navgoco('toggle', false);
-	});
-
-	$('#expandAll').click(function(e) {
-		e.preventDefault();
-		$('#gallery_sel').navgoco('toggle', true);
-	});
-});
-</script>
-			";
+";
 			
 			$assets_down = "			
 <script src='{urlbase}extensions/ImageUpload/js/vendor/jquery.ui.widget.js'></script>
